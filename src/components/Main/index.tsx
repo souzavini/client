f29ec504a7e0c81873b963365cfc15cdb6ff135d
souzavini <viniciuscarr@hotmail.com>
2021-08-10 20:38:48 -0300

    Initial commit from Create Next App

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,11 @@
+import * as S from './styles'
+const Main = () => (
+  <S.Wrapper>
+    <S.Logo src="/img/logo.svg" alt="Imagem de um átomo e React avançado escrito ao lado" />
+    <S.Title>React Avançado</S.Title>
+    <S.Description>Typescript, ReactJS, NextJS e Styled Components</S.Description>
+    <S.Illustration src="/img/hero-illustration.svg" alt="Um desenvolvedor de frente para tela" />
+  </S.Wrapper>
+)
+
+export default Main
